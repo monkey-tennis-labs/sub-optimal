b2c221b4f37e6d8d9c6273c0d51e226622181b3c
Andrzej Prochyra <andrzej@prochyra.io>
2023-12-12 20:49:11 +0000

    Add diver sprite

--- src/game.ts
+++ src/game.ts
@@ -5,9 +5,20 @@ export default class Game extends Phaser.Scene {
     super("game");
   }
 
-  preload() {}
+  preload() {
+    this.load.atlas(
+      "diver",
+      "assets/sprites/hero/diver-swim.png",
+      "assets/sprites/hero/diver-swim.json",
+    );
+  }
 
-  create() {}
+  create() {
+    const width = this.scale.width;
+    const height = this.scale.height;
+
+    this.add.sprite(width * 0.5, height * 0.5, "diver");
+  }
 
   update() {}
 }
