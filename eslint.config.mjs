cdf874ad48f99e647687f1052efd9fa23e0bea77
Andrzej Prochyra <andrzej@prochyra.io>
2024-04-07 21:50:47 +0100

    Migrate to modern typescript-eslint configuration with flat ESLint config format to prepare for ESLint v9
    Followed "Getting Started" guide: https://typescript-eslint.io/getting-started

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    ignores: ["dist/"],
+  },
+  {
+    files: ["**/*.mjs"],
+    extends: [tseslint.configs.disableTypeChecked],
+  },
+);
